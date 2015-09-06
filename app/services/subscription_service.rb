df136eb74d77c10bd8221c79b1af21a2864388f4
James Kiesel <james.kiesel@gmail.com>
2015-09-06 10:46:52 -0700

    Add update subscription method

--- app/services/subscription_service.rb
+++ app/services/subscription_service.rb
@@ -1,9 +1,14 @@
 class SubscriptionService
 
-
   def self.create(subscription:, actor:)
     actor.ability.authorize! :create, subscription
     subscription.tap(&:save!)
+  end
+
+  def self.update(subscription:, params:, actor)
+    actor.ability.authorize! :update, subscription
+    subscription.update params
+    subscription
   end
 
 end
