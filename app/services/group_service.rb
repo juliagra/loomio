92779b540fd618813e1bb810296d6a7d5d1f4ed9
James Kiesel <james.kiesel@gmail.com>
2015-09-06 10:46:32 -0700

    Create trial subscription on new groups

--- app/services/group_service.rb
+++ app/services/group_service.rb
@@ -2,9 +2,12 @@ class GroupService
   def self.create(group:, actor:)
     group.creator = actor
     actor.ability.authorize! :create, group
-    group.default_group_cover = DefaultGroupCover.sample if group.is_parent?
+
     group.save && group.mark_as_setup! && group.add_admin!(actor)
-    group.update default_group_cover: DefaultGroupCover.sample if group.is_parent?
+    if group.is_parent?
+      group.update default_group_cover: DefaultGroupCover.sample,
+                   subscription: Subscription.new(kind: :trial)
+    end
     group
   end
 
